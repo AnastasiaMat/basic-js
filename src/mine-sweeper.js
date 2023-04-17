9a6da0b7c546e8c63a5cee072a9339bcdceee429
AnastasiaMat <42992038+AnastasiaMat@users.noreply.github.com>
2023-04-17 20:44:18 +0300

    Update mine-sweeper.js

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let res = JSON.parse(JSON.stringify(matrix))
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      let counter = 0;
+
+      if (j !== matrix[i].length - 1) {
+        if (matrix[i][j + 1] === true) {
+          counter += 1;
+        }
+      }
+
+      if (j !== 0) {
+        if (matrix[i][j - 1] === true) {
+          counter += 1;
+        }
+      }
+
+      if (i !== matrix.length - 1) {
+        if (matrix[i + 1][j] === true) {
+          counter += 1;
+        }
+      }
+
+      if (i !== 0) {
+        if (matrix[i - 1][j] === true) {
+          counter += 1;
+        }
+      }
+
+      if (i !== 0 && j !== 0) {
+        if (matrix[i - 1][j - 1] === true) {
+          counter += 1;
+        }
+      }
+
+      if (i !== 0 && j !== matrix.length - 1) {
+        if (matrix[i - 1][j + 1] === true) {
+          counter += 1;
+        }
+      }
+
+      if (i !== matrix.length - 1 && j !== 0) {
+        if (matrix[i + 1][j - 1] === true) {
+          counter += 1;
+        }
+      }
+
+      if (i !== matrix.length - 1 && j !== matrix.length - 1) {
+        if (matrix[i + 1][j + 1] === true) {
+          counter += 1;
+        }
+      }
+
+      res[i][j] = counter;
+
+      if (matrix[i][j] === true) {
+        res[i][j] = 1;
+      }
+    }
+  }
+
+  return res;  
 }
 
 module.exports = {
